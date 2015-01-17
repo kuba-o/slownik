f96af1942fdeae9f915b3ca72dffd3992a9a4915
kuba <kuba.ostrowicki@interia.pl>
2015-01-17 20:33:47 +0100

    tab to spaces

--- slownik.cpp
+++ slownik.cpp
@@ -6,42 +6,50 @@
 
 using namespace std;
 
+
+struct slowo {
+  string pl;
+  string en;
+  };
+
 int main(){
-	vector<string> dict;
-	string pl="0";
-	string en;
-	string word;
-	string str;
-	string strVec;
+  vector<slowo> slowo;
 
-	ifstream read;
-	read.open("dict.txt");
-	while (getline(read, str)){
-		strVec = str + "\n";
-		dict.push_back(string(strVec));
-	  cout << str << endl;
-	}
-	read.close();
+  vector<string> dict;
+  string pl="0";
+  string en;
+  string word;
+  string str;
+  string strVec;
 
-	while (1){
-		cout<<"slowo pl:"<<endl;
-			cin>>pl;
-		if (pl=="1")
-			break;
-		else{
-			cout<<"slowo en:"<<endl;
-			cin>>en;
+  ifstream read;
+  read.open("dict.txt");
+  while (getline(read, str)){
+    strVec = str + "\n";
+    dict.push_back(string(strVec));
+    cout << str << endl;
+  }
+  read.close();
 
-			word = pl + ' ' + en + ' ';
+  while (1){
+    cout<<"slowo pl:"<<endl;
+      cin>>pl;
+    if (pl=="1")
+      break;
+    else{
+      cout<<"slowo en:"<<endl;
+      cin>>en;
 
-			dict.push_back(string(word));
-		}
-	}
-	
-	ofstream write;
-	write.open("dict.txt");
-	for (int i=0; i<dict.size(); i++){
-		write<<dict[i];
-	}	
-	return 0;
+      word = pl + ' ' + en + ' ';
+
+      dict.push_back(string(word));
+    }
+  }
+
+  ofstream write;
+  write.open("dict.txt");
+  for (int i=0; i<dict.size(); i++){
+    write<<dict[i];
+  } 
+  return 0;
 }
