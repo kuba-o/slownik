ccfbc82eb3eb62dc149cf263be2c0c891ca55f9e
kuba <kuba.ostrowicki@interia.pl>
2015-01-17 20:26:15 +0100

    mechanizm, slowa bez spacji

---
+++ slownik.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <iterator>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+	vector<string> dict;
+	string pl="0";
+	string en;
+	string word;
+	string str;
+	string strVec;
+
+	ifstream read;
+	read.open("dict.txt");
+	while (getline(read, str)){
+		strVec = str + "\n";
+		dict.push_back(string(strVec));
+	  cout << str << endl;
+	}
+	read.close();
+
+	while (1){
+		cout<<"slowo pl:"<<endl;
+			cin>>pl;
+		if (pl=="1")
+			break;
+		else{
+			cout<<"slowo en:"<<endl;
+			cin>>en;
+
+			word = pl + ' ' + en + ' ';
+
+			dict.push_back(string(word));
+		}
+	}
+	
+	ofstream write;
+	write.open("dict.txt");
+	for (int i=0; i<dict.size(); i++){
+		write<<dict[i];
+	}	
+	return 0;
+}
